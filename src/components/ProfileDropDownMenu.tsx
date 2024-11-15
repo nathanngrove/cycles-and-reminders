dd1c8643331f8808e641ee8a526de5529e1c66b5
Nathan Grove <nathangrove08@gmail.com>
2024-11-14 19:39:27 -0800

    updating

--- src/components/ProfileDropDownMenu.tsx
+++ src/components/ProfileDropDownMenu.tsx
@@ -1,6 +1,6 @@
 "use client";
 import Link from "next/link";
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 
 type ProfileDropDownMenuProps = {
 	username: string;
@@ -11,37 +11,48 @@ function ProfileDropDownMenu({
 	username,
 	deleteSession,
 }: ProfileDropDownMenuProps) {
-	const [isOpen, setIsOpen] = useState(false);
+	const popover = useRef<HTMLUListElement>(null);
 
 	return (
 		<div>
 			<div className="relative">
 				<button
-					className="size-[50px] bg-white rounded-full grid place-items-center text-black font-bold text-2xl cursor-pointer"
-					onClick={() => {
-						setIsOpen((prevState) => !prevState);
-					}}>
+					popoverTarget="popover"
+					className="size-[50px] bg-white rounded-full grid place-items-center text-black font-bold text-2xl cursor-pointer">
 					{username.charAt(0).toUpperCase()}
 				</button>
-				{isOpen ? (
-					<ul className="absolute right-0 bg-neutral-700 rounded-md mt-1">
-						<li className="w-full py-2 px-12 hover:bg-neutral-600 rounded-md cursor-pointer text-center">
-							<Link href="/">Profile</Link>
-						</li>
-						<li className="w-full py-2 px-12 hover:bg-neutral-600 rounded-md cursor-pointer text-center">
-							<Link href="/dashboard">Dashboard</Link>
-						</li>
-						<li>
-							<button
-								className="w-full h-full py-2 px-12 hover:bg-neutral-600 rounded-md"
-								onClick={async () => {
-									await deleteSession();
-								}}>
-								Logout
-							</button>
-						</li>
-					</ul>
-				) : null}
+				<ul
+					ref={popover}
+					popover="auto"
+					id="popover"
+					className="left-[80%] translate-x-[-60%] top-24 bg-neutral-700 rounded-md text-white">
+					<li className="w-full hover:bg-neutral-600 rounded-md cursor-pointer text-center">
+						<Link
+							className="px-12 leading-[2.5rem]"
+							href="/"
+							onClick={() => popover.current?.hidePopover()}>
+							Profile
+						</Link>
+					</li>
+					<li className="w-full  hover:bg-neutral-600 rounded-md cursor-pointer text-center">
+						<Link
+							className="px-12 leading-[2.5rem]"
+							href="/dashboard"
+							onClick={() => popover.current?.hidePopover()}>
+							Dashboard
+						</Link>
+					</li>
+					<li>
+						<button
+							className="w-full h-full py-2 px-12 hover:bg-neutral-600 rounded-md"
+							onClick={async () => {
+								popover.current?.hidePopover();
+								await deleteSession();
+							}}>
+							Logout
+						</button>
+					</li>
+				</ul>
 			</div>
 		</div>
 	);
--- src/components/Tile.tsx
+++ src/components/Tile.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Tile() {
 	return (
-		<div className="flex flex-col bg-neutral-700 p-6 gap-8 rounded-md">
+		<div className="flex flex-col bg-neutral-700 aspect-video p-4 rounded-md lg:text-lg md:text-xl text-2xl">
 			<p className="flex-grow">Drink water and sit and stand</p>
 			<div className="flex items-center">
 				<p className="flex-grow">Frequency</p>
